4d76d7ac043f790bdcce622d67db129474f5cf15
Chris Neal <chrisvneal@gmail.com>
2022-06-26 19:41:06 -1000

    Hook up componentDidMount and connect function

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { fetchPosts } from "../actions";
+import { connect } from "react-redux";
 
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
   render() {
     return <div>Post List</div>;
   }
 }
 
-export default PostList;
+export default connect(null, { fetchPosts })(PostList);
