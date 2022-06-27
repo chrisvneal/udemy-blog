e578edaaca9ac57a80c39c36d16a070e44e0b874
Chris Neal <chrisvneal@gmail.com>
2022-06-26 19:16:12 -1000

    Make fake reducer

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import App from "./components/App";
+import reducers from "./reducers";
+
+const container = document.querySelector("#root");
+
+const root = createRoot(container);
+
+root.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>
+);
