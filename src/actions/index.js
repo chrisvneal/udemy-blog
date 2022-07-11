0080078d267cfc9155bc715d254bdf1014b6b92e
Chris Neal <chrisvneal@gmail.com>
2022-07-10 21:17:14 -1000

    Refactor code to get list of users

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,13 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 import _ from "lodash";
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  // fetch list of posts, when that response is ready (after "await")...
   await dispatch(fetchPosts());
+
+  // ...get each unique user using 'memoize'
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
 };
 
 export const fetchPosts = () => async (dispatch) => {
@@ -14,10 +19,3 @@ export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
-
-// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-
-// const _fetchUser = _.memoize(async (id, dispatch) => {
-//   const response = await jsonPlaceholder.get(`/users/${id}`);
-//   dispatch({ type: "FETCH_USER", payload: response.data });
-// });
